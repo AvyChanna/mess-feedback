d3321db721018850fcc36023d587eed83f254c20
evilbefall <naveengupta10001@gmail.com>
2019-04-28 12:19:24 +0600

    dsf

--- api/auth-login.php
+++ api/auth-login.php
@@ -1,4 +1,31 @@
-// check login
 <?php
- session_start();
- 
+session_start ();
+$_SESSION['message']='';
+$mysqli = new mysqli('localhost', 'root', '','mess') or die("Connect failed: %s\n". $conn -> error);
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	if(true){
+		$username = $mysqli->real_escape_string($_POST['username']);
+		//$password = md5($_POST['password']);
+		$password = $mysqli->real_escape_string($_POST['password']);
+		$sql = "SELECT passwordhash FROM users WHERE username='".$username."'";
+		$result = mysqli_query($mysqli, $sql);
+		if (($mysqli->query($sql)==true)){
+			if (mysqli_num_rows($result) > 0) {
+				while($row = mysqli_fetch_assoc($result)) {
+					if($row["passwordhash"]==$password){
+						if($row["status"]=="admin"){
+							header('location: ../admin.php');
+						}
+						if($row["status"]=="manager"){
+							header('location: ../manager.php');
+						}
+						else
+							header('location: ../home.php');
+					}
+				 }
+			}
+			//mysqli_close($mysqli);
+		} else die($mysqli->error);
+	}
+}
+?>
--- api/auth-signup.php
+++ api/auth-signup.php
@@ -1,1 +1,22 @@
 // sign up a user
+<?php
+$_SESSION['message']='';
+$mysqli = new mysqli('localhost', 'root', '','mess') or die("Connect failed: %s\n". $conn -> error);
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	if($_POST['password']==$_POST['confpassword']){
+		$username = $mysqli->real_escape_string($_POST['username']);
+		$fullname = $mysqli->real_escape_string($_POST['name']);
+		$rollno = $mysqli->real_escape_string($_POST['rollno']);
+		$rollno = (int)$rollno;
+		//$password = md5($_POST['password']);
+		$password = $mysqli->real_escape_string($_POST['password']);
+		$sql = "INSERT INTO users (username, `name`, rollno, passwordhash)". "Values ('".$username."', '".$fullname."', ".$rollno.", '".$password."')";
+
+		if ($mysqli->query($sql)==true){
+			$_SESSION['message']= 'Registration successful';
+			header('location: ../login.php');
+		} else die($mysqli->error);
+	}
+}
+?>
+

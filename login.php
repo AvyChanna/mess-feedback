b55ce30e307103133cf4c4420e5211b07b42cf4b
evilbefall <naveengupta10001@gmail.com>
2019-05-02 00:11:21 +0600

    as

--- login.php
+++ login.php
@@ -33,7 +33,41 @@ if(  (isset($_SESSION["loggedin"]))   &&   ($_SESSION["loggedin"]=="t")){
 					<hr>
 				</div>
 				<div class="col-12 col-sm-8 col-md-6 col-xl-4 offset-0 offset-xl-4 offset-md-3 offset-sm-2 pb-5">
-					<form action="/api/auth-login.php" method="POST">
+				<?php
+				if(  (!isset($_SESSION["loggedin"]))   ||   ($_SESSION["loggedin"]=="f")){
+					if($_SERVER['REQUEST_METHOD']=='POST'){
+						$mysqli = new mysqli('localhost', 'root', '','mess') or die("Connect failed: %s\n". $conn -> error);
+						$username = $mysqli->real_escape_string($_POST['username']);
+						//$password = md5($_POST['password']);
+						$password = $mysqli->real_escape_string($_POST['password']);
+						$sql = "SELECT * FROM users WHERE username='".$username."'";
+						$result = mysqli_query($mysqli, $sql);
+						if (($mysqli->query($sql))){
+							if (mysqli_num_rows($result) == 1) {
+								$row = mysqli_fetch_assoc($result);
+								if ($row == false) die();
+								if($row["passwordhash"]==$password){
+									$_SESSION["loggedin"]="t";
+									$_SESSION["status"]=$row["status"];
+									$_SESSION["username"]=$row["username"];
+									$_SESSION["name"]=$row["name"];
+									$_SESSION["rollno"]=$row["rollno"];
+									$_SESSION["mess"]=$row["mess"];
+									$_SESSION["password"]=$row["passwordhash"];
+									header('location: ../home.php');
+									die();
+								} else echo ("Password is incorrect");
+							} else 
+							{echo("Username is incorrect");}
+							//mysqli_close($mysqli);
+						} 
+					}
+				} else if(  (isset($_SESSION["loggedin"]))   &&   ($_SESSION["loggedin"]=="t")){
+					header('Location: ../home.php');
+					die();
+				}
+				?>
+					<form action="#" method="POST">
 						<div class="form-group">
 							<label for="uname">Username:</label>
 							<div class="input-group mb-3">
